1d13e8b9292f0c992ab84ce18de2889172b94372
endlmk <1269467+endlmk@users.noreply.github.com>
2024-06-10 22:00:40 +0900

    34

---
+++ 34.py
@@ -0,0 +1,34 @@
+
+with open('neko.txt.mecab') as file:
+    result = []
+    line_m =[]
+    for line in file:
+        if line == 'EOS\n':
+            if len(line_m) > 0:
+                result.append(line_m)
+                line_m =[]
+            continue
+        
+        features = line.split('\t')
+        pos = features[4].split('-')
+        mapping = {
+            'surface': features[0],
+            'base': features[3],
+            'pos': pos[0],
+            'pos1': pos[1] if len(pos) > 1 else ''
+        }
+        line_m.append(mapping)
+
+for l in result:
+    concat_norm = ""
+    for m in l:
+        if m['pos'] == '名詞':
+            concat_norm = concat_norm + m['surface']
+        else:
+            if concat_norm != "":
+                print(concat_norm)
+                concat_norm = ""
+
+    if concat_norm != "":
+        print(concat_norm)
+        concat_norm = ""
