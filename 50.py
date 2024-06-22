9d49ebc0288ab49aefeeff60d839f391c29aff00
endlmk <1269467+endlmk@users.noreply.github.com>
2024-06-22 22:12:55 +0900

    50

---
+++ 50.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('data_source/news+aggregator/newsCorpora.csv', sep='\t', header=None)
+df.columns = ['ID','TITLE','URL','PUBLISHER','CATEGORY','STORY','HOSTNAME','TIMESTAMP']
+df_filter = df[df['PUBLISHER'].isin(['Reuters', 'Huffington Post', 'Businessweek', 'Contactmusic.com', 'Daily Mail'])]
+
+train, valid_test = train_test_split(df_filter, test_size=0.2)
+valid, test = train_test_split(valid_test, test_size=0.5)
+
+train.to_csv('data_source/50/train.txt', sep='\t', index=False)
+valid.to_csv('data_source/50/valid.txt', sep='\t', index=False)
+test.to_csv('data_source/50/test.txt', sep='\t', index=False)
+
