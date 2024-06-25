f4e47a022b43e04b2452c874096f9456acf145e3
endlmk <1269467+endlmk@users.noreply.github.com>
2024-06-25 22:55:52 +0900

    51

--- 50.py
+++ 50.py
@@ -5,8 +5,8 @@ df = pd.read_csv('data_source/news+aggregator/newsCorpora.csv', sep='\t', header
 df.columns = ['ID','TITLE','URL','PUBLISHER','CATEGORY','STORY','HOSTNAME','TIMESTAMP']
 df_filter = df[df['PUBLISHER'].isin(['Reuters', 'Huffington Post', 'Businessweek', 'Contactmusic.com', 'Daily Mail'])]
 
-train, valid_test = train_test_split(df_filter, test_size=0.2)
-valid, test = train_test_split(valid_test, test_size=0.5)
+train, valid_test = train_test_split(df_filter, test_size=0.2, stratify=df_filter['CATEGORY'])
+valid, test = train_test_split(valid_test, test_size=0.5, stratify=valid_test['CATEGORY'])
 
 train.to_csv('data_source/50/train.txt', sep='\t', index=False)
 valid.to_csv('data_source/50/valid.txt', sep='\t', index=False)
---
+++ 51.py
@@ -0,0 +1,27 @@
+
+#%%
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+import re
+import string
+
+def preprocess(x: str):
+    return x
+
+train = pd.read_csv('data_source/50/train.txt', sep='\t')
+valid = pd.read_csv('data_source/50/valid.txt', sep='\t')
+test = pd.read_csv('data_source/50/test.txt', sep='\t')
+combined = pd.concat([train, valid, test])
+
+preprocessed = combined['TITLE'].map(lambda x: preprocess(x))
+vectorizer = TfidfVectorizer()
+feature = vectorizer.fit_transform(combined['TITLE'])
+feature_df = pd.DataFrame(feature.toarray(), columns=vectorizer.get_feature_names_out())
+
+feature_train = feature_df.iloc[:len(train)].reset_index(drop=True)
+feature_valid = feature_df.iloc[len(train):len(train)+ len(test)].reset_index(drop=True)
+feature_test = feature_df.iloc[len(train)+ len(test):].reset_index(drop=True)
+
+feature_train.to_csv('data_source/51/feature_train.txt', sep='\t', index=False)
+feature_valid.to_csv('data_source/51/feature_valid.txt', sep='\t', index=False)
+feature_test.to_csv('data_source/51/feature_test.txt', sep='\t', index=False)
