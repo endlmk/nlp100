cdf72ffbc19a0d9d92dd9e519345cd3b57dff75c
endlmk <1269467+endlmk@users.noreply.github.com>
2024-06-26 22:05:08 +0900

    remove comment.

--- 53.py
+++ 53.py
@@ -1,4 +1,3 @@
-#%%
 import joblib
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
@@ -12,9 +11,6 @@ feature_test = pd.read_csv('data_source/51/feature_test.txt', sep='\t')
 model: LogisticRegression = joblib.load('data_source/52/model.joblib')
 pred_proba = model.predict_proba(feature_test)
 
-
-# %%
 pred = pd.DataFrame(pred_proba, columns=model.classes_)
 
 pred.to_csv('data_source/53/pred.txt', sep='\t', index=False)
-# %%
