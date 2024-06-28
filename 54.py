6e170547ae2141036bdcb806ec416eb8d6735494
endlmk <1269467+endlmk@users.noreply.github.com>
2024-06-28 21:39:10 +0900

    Fix 54, 55.

--- 54.py
+++ 54.py
@@ -3,18 +3,18 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-valid = pd.read_csv('data_source/50/valid.txt', sep='\t')
+train = pd.read_csv('data_source/50/train.txt', sep='\t')
 test = pd.read_csv('data_source/50/test.txt', sep='\t')
 
-feature_valid = pd.read_csv('data_source/51/feature_valid.txt', sep='\t')
+feature_train = pd.read_csv('data_source/51/feature_train.txt', sep='\t')
 feature_test = pd.read_csv('data_source/51/feature_test.txt', sep='\t')
 
 model: LogisticRegression = joblib.load('data_source/52/model.joblib')
 
+pred_train = model.predict(feature_train)
+accuracy_train = accuracy_score(train['CATEGORY'], pred_train)
+print('Accuracy train: ', accuracy_train)
+
 pred_test = model.predict(feature_test)
 accuracy_test = accuracy_score(test['CATEGORY'], pred_test)
 print('Accuracy test: ', accuracy_test)
-
-pred_valid = model.predict(feature_valid)
-accuracy_valid = accuracy_score(valid['CATEGORY'], pred_valid)
-print('Accuracy valid: ', accuracy_valid)
--- 55.py
+++ 55.py
@@ -3,18 +3,18 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 
-valid = pd.read_csv('data_source/50/valid.txt', sep='\t')
+train = pd.read_csv('data_source/50/train.txt', sep='\t')
 test = pd.read_csv('data_source/50/test.txt', sep='\t')
 
-feature_valid = pd.read_csv('data_source/51/feature_valid.txt', sep='\t')
+feature_train = pd.read_csv('data_source/51/feature_train.txt', sep='\t')
 feature_test = pd.read_csv('data_source/51/feature_test.txt', sep='\t')
 
 model: LogisticRegression = joblib.load('data_source/52/model.joblib')
 
+pred_train = model.predict(feature_train)
+accuracy_train = confusion_matrix(train['CATEGORY'], pred_train)
+print('Confusion Matirx train:\n', accuracy_train)
+
 pred_test = model.predict(feature_test)
 accuracy_test = confusion_matrix(test['CATEGORY'], pred_test)
 print('Confusion Matirx test:\n', accuracy_test)
-
-pred_valid = model.predict(feature_valid)
-accuracy_valid = confusion_matrix(valid['CATEGORY'], pred_valid)
-print('Confusion Matirx valid:\n', accuracy_valid)
