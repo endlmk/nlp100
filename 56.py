a78cac4ec2d9a4d7d9382c99b3b526f7e03720b5
endlmk <1269467+endlmk@users.noreply.github.com>
2024-06-28 21:52:06 +0900

    56

---
+++ 56.py
@@ -0,0 +1,20 @@
+import joblib
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+
+train = pd.read_csv('data_source/50/train.txt', sep='\t')
+test = pd.read_csv('data_source/50/test.txt', sep='\t')
+
+feature_train = pd.read_csv('data_source/51/feature_train.txt', sep='\t')
+feature_test = pd.read_csv('data_source/51/feature_test.txt', sep='\t')
+
+model: LogisticRegression = joblib.load('data_source/52/model.joblib')
+
+pred_train = model.predict(feature_train)
+accuracy_train = classification_report(train['CATEGORY'], pred_train)
+print('Classification Report train:\n', accuracy_train)
+
+pred_test = model.predict(feature_test)
+accuracy_test = classification_report(test['CATEGORY'], pred_test)
+print('Classification Report test:\n', accuracy_test)
