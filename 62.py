178b13ebcf74f411e3e6a9bc63a44341e3cf587b
endlmk <1269467+endlmk@users.noreply.github.com>
2024-07-02 21:06:07 +0900

    62

---
+++ 62.py
@@ -0,0 +1,8 @@
+#%%
+from gensim.models import KeyedVectors
+
+model = KeyedVectors.load_word2vec_format('data_source/60/GoogleNews-vectors-negative300.bin', binary=True)
+
+#%%
+print(model.most_similar('United_States'))
+
